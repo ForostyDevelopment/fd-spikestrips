a2d7cf6891933cf5be46a7e4812d13aa9845dc35
ForostyDevelopment <108974768+ForostyDevelopment@users.noreply.github.com>
2022-12-19 17:51:23 +0100

    fixed the removeitem

--- fd-spikestrips/server/cl_server.lua
+++ fd-spikestrips/server/cl_server.lua
@@ -8,4 +8,11 @@ RegisterNetEvent('fd-spikestrips:server:SyncSpikes', function(table)
     TriggerClientEvent('fd-spikestrips:client:SyncSpikes', -1, table)
 end)
 
+RegisterNetEvent('fd-spikestrips:server:removeItem', function()
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    Player.Functions.RemoveItem('spikestrip', 1)
+    TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items['spikestrip'], "remove", 1)
+    TriggerClientEvent('fd-spawnstrips:client:spawnSpike', source) 
+end)
 
