37714c9665efa8e80df9a45461fa2424e59f0b98
ForostyDevelopment <108974768+ForostyDevelopment@users.noreply.github.com>
2022-12-19 17:51:56 +0100

    removed the ,

--- fd-spikestrips/config.lua
+++ fd-spikestrips/config.lua
@@ -2,5 +2,5 @@ Config = {}
 
 Config.AllowSpikesOnlyForPolice = false -- If spikes are only usable by police.
 Config.MaxSpikes = 5 -- How many spikes a player can place at the same time
-Config.InstantlyPop = 1, -- 1 == It will instantly pop the tires, false = It will not instantly pop the tires.
+Config.InstantlyPop = 1 -- 1 == It will instantly pop the tires, false = It will not instantly pop the tires.
 Config.DeleteTime = 30 -- How many seconds for the spikestrip to delete after it has been placed (In seconds). 
